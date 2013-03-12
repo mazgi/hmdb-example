775a7d6a90cd00c60361ab1f6f62486a2089d856
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-03-13 00:44:51 +0900

    Create Xcode project.

---
+++ HMDBExample4IOS/HMDBExample4IOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HMDBExample4IOS
+//
+//  Created by Matsuki Hidenori on 3/13/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HMDBExample4IOS/HMDBExample4IOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HMDBExample4IOS
+//
+//  Created by Matsuki Hidenori on 3/13/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
