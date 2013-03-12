e8154a73a8d9b203a61540f09a00cddc0df0abb3
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-03-13 00:46:27 +0900

    Create Xcode project for MacOSX.

---
+++ HMDBExample4MacOSX/HMDBExample4MacOSX/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HMDBExample4MacOSX
+//
+//  Created by Matsuki Hidenori on 3/13/13.
+//  Copyright (c) 2013 Matsuki Hidenori. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
