33c4c8b774ef38764b0130241d291b1ee3c5d4ac
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-03-19 19:07:05 +0900

    Fix main function for hmdb interfaces.

--- HMDBExample4MacOSX/HMDBExample4MacOSX/main.mm
+++ HMDBExample4MacOSX/HMDBExample4MacOSX/main.mm
@@ -9,14 +9,34 @@
 #import <Foundation/Foundation.h>
 #include "hmdb.hpp"
 
+class Driver {
+public:
+    void run()
+    {
+        using hmdb::HMDatabase;
+        using hmdb::HMDatabaseRef;
+        using hmdb::HMError;
+        using hmdb::HMResultSet;
+        HMError *err = nullptr;
+        HMResultSet *ret = nullptr;
+
+        HMDatabaseRef db(new HMDatabase("/tmp/tmp.db"));
+        db->open();
+        db->executeQueryWithResults(
+                         err,
+                         ret,
+                         "SELECT * FROM T WHERE KEY=? OR VALUE IN (?, ?, ?) OR SCORE > ?",
+                         1, "value1", "value2", "value3", 1.0
+                         );
+        db->close();
+    }
+};
+
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
-        using hmdb::HMDatabase;
-        using hmdb::HMDatabaseRef;
-        HMDatabaseRef db(new HMDatabase("/tmp/tmp.db"));
-        db->open();
-        db->close();
+        Driver driver;
+        driver.run();
     }
     return 0;
 }
