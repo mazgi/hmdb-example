4efcbcb7ef5636c96b033628032bbee82ee7652f
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-03-20 09:40:59 +0900

    Rename result classes again.

--- HMDBExample4MacOSX/HMDBExample4MacOSX/main.mm
+++ HMDBExample4MacOSX/HMDBExample4MacOSX/main.mm
@@ -16,7 +16,7 @@ public:
         using hmdb::HMDatabase;
         using hmdb::HMDatabaseRef;
         using hmdb::HMError;
-        using hmdb::HMRecordSet;
+        using hmdb::HMRecordReader;
 
         HMError *err = nullptr;
         HMDatabaseRef db(new HMDatabase("/tmp/tmp.db"));
@@ -47,10 +47,13 @@ public:
                                                          ));
         }
         {
-            HMRecordSet *ret = nullptr;
+            HMRecordReader *reader = nullptr;
+            while (reader->next()) {
+//                reader[0];
+            }
             HMLog("RECORD:READED[%d]", db->executeQueryForRead(
                                                                err,
-                                                               ret,
+                                                               reader,
                                                                "SELECT * FROM SAMPLE_TABLE"
                                                                " WHERE DBL_COL=?"
                                                                " OR INT_COL > ?"
@@ -59,14 +62,6 @@ public:
                                                                " OR STR_COL IN(?)",
                                                                1.0, 2, nullptr, "str4", std::string("str5")
                                                                ));
-            HMError *fetchErr = nullptr;
-            while (ret->nextRow(fetchErr)) {
-                if (fetchErr) {
-                    //err
-                    break;
-                }
-                ;
-            }
         }
         for (int i = 0; i < 10; i++) {
             double d = i + 0.1;
