885fcc88a593b0818609f23719e99b38b357cf41
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-03-24 00:03:14 +0900

    Implement function for C++98 and "Mossaroid".

--- HMDBExample4MacOSX/HMDBExample4MacOSX/main.mm
+++ HMDBExample4MacOSX/HMDBExample4MacOSX/main.mm
@@ -11,21 +11,118 @@
 
 class Driver {
 public:
-    void run()
+    void run98()
     {
-#if HMDB_CXX_DIALECT_CXX11
-        using hmdb::HMDatabaseRef;
-#endif
+#if !HMDB_CXX_DIALECT_CXX11
         using hmdb::HMDatabase;
         using hmdb::HMError;
         using hmdb::HMRecordReader;
 
         HMError *err = HMDB_NULL;
-#if HMDB_CXX_DIALECT_CXX11
-        HMDatabaseRef db(new HMDatabase("/tmp/tmp.db"));
-#else
-        HMDatabase *db = new HMDatabase("/tmp/tmp.db");
+        HMDatabase* db = new HMDatabase("/tmp/tmp98.db");
+
+        HMLog("DB:OPENED[%d]", db->open());
+        HMLog("TABLE:DROPED[%d]", db->executeFormattedQuery(err, "DROP TABLE IF EXISTS SAMPLE_TABLE"));
+        HMLog("TABLE:CREATED[%d]", db->executeFormattedQuery(err,
+                                                             "CREATE TABLE SAMPLE_TABLE("
+                                                             "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                                             " DBL_COL REAL,"
+                                                             " INT_COL INT,"
+                                                             " NULL_COL NONE,"
+                                                             " CHAR_COL TEXT,"
+                                                             " STR_COL TEXT"
+                                                             ")"
+                                                             ));
+        for (int i = 0; i < 10; i++) {
+            HMLog("RECORD:CREATED[%d]", db->executeFormattedQuery(err,
+                                                                  "INSERT INTO SAMPLE_TABLE("
+                                                                  " DBL_COL,"
+                                                                  " INT_COL,"
+                                                                  " NULL_COL,"
+                                                                  " CHAR_COL,"
+                                                                  " STR_COL"
+                                                                  ")"
+                                                                  "VALUES(%f, %d, %s, %s, %s)",
+                                                                  0.0, i, HMDB_NULL, "char*[0]", "str[0]"
+                                                                  ));
+        }
+        {
+            HMRecordReader *reader = HMDB_NULL;
+            HMLog("RECORD:READED[%d]", db->executeFormattedQueryForRead(
+                                                                        err,
+                                                                        reader,
+                                                                        "SELECT * FROM SAMPLE_TABLE"
+                                                                        " WHERE DBL_COL=%f"
+                                                                        " OR INT_COL>%d"
+                                                                        " OR NULL_COL = %s"
+                                                                        " OR CHAR_COL = %s"
+                                                                        " OR STR_COL IN(%s)",
+                                                                        1.0, 2, "HMDB_NULL", "str", "str5"
+                                                                        ));
+            while (reader && reader->next()) {
+                HMLog("DBL_COL:%f", reader->doubleValue("DBL_COL"));
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            double d = i + 0.1;
+            HMLog("RECORD:UPDATED[%d]", db->executeFormattedQuery(err,
+                                                                  "UPDATE SAMPLE_TABLE"
+                                                                  " SET DBL_COL = %f"
+                                                                  " WHERE INT_COL = %d",
+                                                                  d, i));
+        }
+        {
+            HMRecordReader *reader = HMDB_NULL;
+            HMLog("RECORD:READED[%d]", db->executeFormattedQueryForRead(
+                                                                        err,
+                                                                        reader,
+                                                                        "SELECT * FROM SAMPLE_TABLE"
+                                                                        " WHERE DBL_COL=%f"
+                                                                        " OR INT_COL > %d"
+                                                                        " OR NULL_COL = %s"
+                                                                        " OR CHAR_COL = %s"
+                                                                        " OR STR_COL IN(%s)",
+                                                                        1.0, 2, HMDB_NULL, "str4", "str5"
+                                                                        ));
+            while (reader && reader->next()) {
+                HMLog("ID:%d", reader->intValue("ID"));
+                HMLog("DBL_COL:%f", reader->doubleValue("DBL_COL"));
+                HMLog("INT_COL:%d", reader->intValue("INT_COL"));
+                HMLog("DBL_COL:%s", reader->textValue("NULL_COL"));
+                HMLog("DBL_COL:%s", reader->textValue("CHAR_COL"));
+                HMLog("DBL_COL:%s", reader->textValue("STR_COL"));
+            }
+        }
+        HMLog("BEGIN TRANSACTION[%d]", db->beginTransaction());
+        for (int i = 0; i < 10; i += 2) {
+            HMLog("RECORD:DELETED[%d]", db->executeFormattedQuery(err,
+                                                                  "DELETE FROM SAMPLE_TABLE"
+                                                                  " WHERE INT_COL = %d",
+                                                                  i));
+        }
+        HMLog("COMMIT TRANSACTION[%d]", db->commitTransaction());
+        HMLog("BEGIN TRANSACTION[%d]", db->beginTransaction());
+        for (int i = 1; i < 10; i += 2) {
+            HMLog("RECORD:DELETED[%d]", db->executeFormattedQuery(err,
+                                                                  "DELETE FROM SAMPLE_TABLE"
+                                                                  " WHERE INT_COL = %d",
+                                                                  i));
+        }
+        HMLog("ROLLBACK TRANSACTION[%d]", db->rollbackTransaction());
+        HMLog("DB:CLOSED[%d]", db->close());
 #endif
+    }
+    void run11()
+    {
+#if HMDB_CXX_DIALECT_CXX11
+        using hmdb::HMDatabaseRef;
+        using hmdb::HMDatabase;
+        using hmdb::HMError;
+        using hmdb::HMRecordReader;
+
+        HMError *err = HMDB_NULL;
+        HMDatabaseRef db(new HMDatabase("/tmp/tmp11.db"));
+
         HMLog("DB:OPENED[%d]", db->open());
         HMLog("TABLE:DROPED[%d]", db->executeQuery(err, "DROP TABLE IF EXISTS SAMPLE_TABLE"));
         HMLog("TABLE:CREATED[%d]", db->executeQuery(err,
@@ -39,7 +136,7 @@ public:
                                                     ")"
                                                     ));
         for (int i = 0; i < 10; i++) {
-#if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
+#   if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
             HMLog("RECORD:CREATED[%d]", db->executeQuery(err,
                                                          "INSERT INTO SAMPLE_TABLE("
                                                          " DBL_COL,"
@@ -51,11 +148,11 @@ public:
                                                          "VALUES(?, ?, ?, ?, ?)",
                                                          0.0, i, HMDB_NULL, "char*[0]", std::string("str[0]")
                                                          ));
-#endif
+#   endif
         }
         {
             HMRecordReader *reader = HMDB_NULL;
-#if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
+#   if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
             HMLog("RECORD:READED[%d]", db->executeQueryForRead(
                                                                err,
                                                                reader,
@@ -67,24 +164,24 @@ public:
                                                                " OR STR_COL IN(?)",
                                                                1.0, 2, HMDB_NULL, "str4", std::string("str5")
                                                                ));
-#endif
+#   endif
             while (reader && reader->next()) {
                 HMLog("DBL_COL:%f", reader->doubleValue("DBL_COL"));
             }
         }
         for (int i = 0; i < 10; i++) {
-#if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
+#   if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
             double d = i + 0.1;
             HMLog("RECORD:UPDATED[%d]", db->executeQuery(err,
                                                          "UPDATE SAMPLE_TABLE"
                                                          " SET DBL_COL = ?"
                                                          " WHERE INT_COL = ?",
                                                          d, i));
-#endif
+#   endif
         }
         {
             HMRecordReader *reader = HMDB_NULL;
-#if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
+#   if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
             HMLog("RECORD:READED[%d]", db->executeQueryForRead(
                                                                err,
                                                                reader,
@@ -96,7 +193,7 @@ public:
                                                                " OR STR_COL IN(?)",
                                                                1.0, 2, HMDB_NULL, "str4", std::string("str5")
                                                                ));
-#endif
+#   endif
             while (reader && reader->next()) {
                 HMLog("ID:%d", reader->intValue("ID"));
                 HMLog("DBL_COL:%f", reader->doubleValue("DBL_COL"));
@@ -108,27 +205,25 @@ public:
         }
         HMLog("BEGIN TRANSACTION[%d]", db->beginTransaction());
         for (int i = 0; i < 10; i += 2) {
-#if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
+#   if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
             HMLog("RECORD:DELETED[%d]", db->executeQuery(err,
                                                          "DELETE FROM SAMPLE_TABLE"
                                                          " WHERE INT_COL = ?",
                                                          i));
-#endif
+#   endif
         }
         HMLog("COMMIT TRANSACTION[%d]", db->commitTransaction());
         HMLog("BEGIN TRANSACTION[%d]", db->beginTransaction());
         for (int i = 1; i < 10; i += 2) {
-#if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
+#   if HMDB_CXX_FEATURE_CXX_VARIADIC_TEMPLATES
             HMLog("RECORD:DELETED[%d]", db->executeQuery(err,
                                                          "DELETE FROM SAMPLE_TABLE"
                                                          " WHERE INT_COL = ?",
                                                          i));
-#endif
+#   endif
         }
         HMLog("ROLLBACK TRANSACTION[%d]", db->rollbackTransaction());
         HMLog("DB:CLOSED[%d]", db->close());
-#if !HMDB_CXX_DIALECT_CXX11
-        delete db;
 #endif
     }
 };
@@ -136,7 +231,11 @@ public:
 int main(int argc, const char * argv[])
 {
     Driver driver;
-    driver.run();
+#if !HMDB_CXX_DIALECT_CXX11
+    driver.run98();
+#else
+    driver.run11();
+#endif
     return 0;
 }
 
